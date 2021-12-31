a7013fb69d841b05736c4cc6a8701f9936b85ccb
jise8893 <60957575+jise8893@users.noreply.github.com>
2021-12-31 13:36:39 +0900

    Create input.cpp

---
+++ src/input.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    // 입력을 주다가 안줄 때
+    
+    while(cin>>n)
+    {
+        
+    }
+    /*
+    while(scanf("%d",&n)!=EOF){
+        
+    }
+    */
+}
