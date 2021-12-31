53545f74017bd1184686dbe9e1a7ff571b4a8021
jise8893 <60957575+jise8893@users.noreply.github.com>
2021-12-31 13:55:49 +0900

    Create string.cpp

---
+++ src/string.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+string dop="life is limited";
+int main(){
+    cout<< dop<<'\n';
+    cout<< dop.substr(0,3)<<'\n';
+    reverse(dop.begin(),dopa.end());
+    cout<<dop<<'\n';
+}
