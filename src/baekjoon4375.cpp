d04de2d0649d8cf667584c4fe1ecc7b003849a96
jise8893 <60957575+jise8893@users.noreply.github.com>
2022-01-24 22:12:30 +0900

    Create baekjoon4375.cpp

---
+++ src/baekjoon4375.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    unsigned long long p=0;
+    
+    while(cin>>p)
+    {
+        unsigned long long cnt=1;
+        int ret=1;    
+        while(true)
+        {
+            if(cnt%p==0)
+            {
+              cout<<ret<<"\n"; 
+              break;
+            }
+            ret++;
+            cnt=cnt*10+1;
+            cnt%=p;
+        }
+        
+    }
+    
+}
